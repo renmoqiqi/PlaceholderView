f392e55e196c68e0127d2c1f01804c5fa30c2a2e
sevens <penghe@wondersgroup.com>
2016-06-28 13:35:12 +0800

    方法格式化
    方法格式化

--- PlaceholdViewDemo/Classes/UIViewController+PlaceholderView.h
+++ PlaceholdViewDemo/Classes/UIViewController+PlaceholderView.h
@@ -24,7 +24,13 @@ NS_ASSUME_NONNULL_BEGIN
  *  @param view             被添加的View
  *  @param tapButtonBlock   点击按钮的回调
  */
-- (void)showPlaceholderViewTitle:(nullable NSString *)title subTitle:(nullable NSString *)subTitle  tapButtonBgImage:(nullable UIImage *)tapButtonBgImage tapButtonTitle:(nullable NSString *)tapButtonTitle placeholderImage:(nullable UIImage *)placeholderImage inView:(nullable UIView *)view tapButtonBlock:(void(^)(void))tapButtonBlock;
+- (void)showPlaceholderViewTitle:(nullable NSString *)title
+                        subTitle:(nullable NSString *)subTitle
+                tapButtonBgImage:(nullable UIImage *)tapButtonBgImage
+                  tapButtonTitle:(nullable NSString *)tapButtonTitle
+                placeholderImage:(nullable UIImage *)placeholderImage
+                          inView:(nullable UIView *)view
+                  tapButtonBlock:(void(^)(void))tapButtonBlock;
 
 /**
  *  功能：添加默认视图
@@ -35,7 +41,10 @@ NS_ASSUME_NONNULL_BEGIN
  *  @param view             被添加的View
  */
 
-- (void)showPlaceholderViewTitle:(nullable NSString *)title subTitle:(nullable NSString *)subTitle  placeholderImage:(nullable UIImage *)placeholderImage inView:(nullable UIView *)view ;
+- (void)showPlaceholderViewTitle:(nullable NSString *)title
+                        subTitle:(nullable NSString *)subTitle
+                placeholderImage:(nullable UIImage *)placeholderImage
+                          inView:(nullable UIView *)view ;
 
 /**
  *  功能：隐藏加载失败的View
